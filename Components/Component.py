fa538379fdb3ce6564dcf697473b437c3f880602
Cat Flynn <mewtwo97@hotmail.com>
2016-06-06 14:17:52 +0100

    add static find to Component

--- Components/Component.py
+++ Components/Component.py
@@ -1,8 +1,19 @@
 class Component:
+
+	List = []
+
+	@staticmethod
+	def find(tag):
+		for component in Component.List:
+			if component.tag == tag:
+			    return component
+		return None
+
 	def __init__(self, entity, tag):
 		self.entity = entity
 		self.enabled = True
 		self.tag = tag
+		Component.List.append(self)
 
 	def update(self):
 		return
