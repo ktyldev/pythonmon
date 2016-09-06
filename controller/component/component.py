90acc739a08180db64be2ae80de9d00fa5951674
Cat Flynn <mewtwo97@hotmail.com>
2016-09-06 17:48:13 +0100

    update component usage documentation

--- controller/component/component.py
+++ controller/component/component.py
@@ -1,7 +1,7 @@
 """
-COMPONENT USAGE
+##~COMPONENT USAGE~##
 
-This is where all game logic is defined. Components should only update themselves and the entity they are attached
+Game logic is defined in 'Components'. Components should only update themselves and the entity they are attached
 to. On rare occasions it may be necessary to access other components from inside a component's update method. This
 should be achieved via ```self.scene.find_entity(%entity_name%).get_component(%component_name%)```.
 
@@ -14,18 +14,23 @@ Every component should have a parameter-less constructor defined.
 
 component.update()
 
-This method will be called once per tick. It is used to update the state of the Component and the entity to which
+update() will be called once per tick. It is used to update the state of the Component and the entity to which
 the component is attached.
 
 component.start()
 
-This method is called once at the start of the scene. It is used to prepare the state of the component using data
+start() is called once at the start of the scene. It is used to prepare the state of the component using data
 that is only available at runtime.
 
 component.load_data(data)
 
-This method is called before the start of the scene. It is used to load data from the scene JSON file. The data is
+load_data() is called before the start of the scene. It is used to load data from the scene JSON file. The data is
 an array which can be used with index arguments to assign data to class members.
+
+##~ADDING COMPONENTS~##
+
+To add a component, add a new file in the 'controller/component/' folder and subclass Component. Construct your class
+and then add a line to 'controller/component/__init__.py' to put it into the 'controller.component' namespace.
 """
 
 
@@ -37,6 +42,12 @@ class Component:
 
     @staticmethod
     def find(tag):
+        """
+        Find the component with the specified tag. This will not work correctly if multiple components have the same
+        tag. Returns None in the case that no components have the specified tag.
+        :param tag:
+        :return:
+        """
         for component in Component.List:
             if component.tag == tag:
                 return component
@@ -48,6 +59,7 @@ class Component:
         self.tag = ''
         self.entity = None
         self.started = False
+
         Component.List.append(self)
 
     def start(self):
